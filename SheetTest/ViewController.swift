82c76dad172e39cc709401cdc5f6d33fc8230bdc
KING <king@KINGs-MacBook-Pro.local>
2017-03-17 00:21:19 +0100

    working commit

---
+++ SheetTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SheetTest
+//
+//  Created by KING on 2017. 03. 17..
+//  Copyright © 2017. PANYOR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
